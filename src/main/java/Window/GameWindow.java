64444eee425691a824b64ceffe67f423dc7c9319
kakao95490 <82544164+kakao95490@users.noreply.github.com>
2023-04-08 14:43:11 +0200

    ça suit le tuto

---
+++ src/main/java/Window/GameWindow.java
@@ -0,0 +1,17 @@
+package Window;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame gameWindow;
+    public GameWindow(String gameWindowName,GamePanel gamePanel){
+        gameWindow=new JFrame();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setTitle(gameWindowName);
+        gameWindow.setSize(1920,1080);
+        gameWindow.add(gamePanel);
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
+
+    }
+}
--- src/main/java/Window/Main.java
+++ src/main/java/Window/Main.java
@@ -3,11 +3,7 @@ package Window;
 import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
-        JFrame window=new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setTitle("Please...SURVIVE!");
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+        Game pls = new Game();
     }
 
 }
