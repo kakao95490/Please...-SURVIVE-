6f92e2408aaa0f6e0a3e69bfd94eb37ef7349d08
kakao95490 <82544164+kakao95490@users.noreply.github.com>
2023-04-08 13:58:52 +0200

    First

---
+++ src/main/java/Window/Main.java
@@ -0,0 +1,13 @@
+package Window;
+
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        JFrame window=new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Please...SURVIVE!");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+}
